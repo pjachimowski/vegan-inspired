7f4d948c4caaa322d2e1cdc12882e9b2e25d2e97
pjachimowski <p.jachimowski89@gmail.com>
2019-12-11 23:33:13 +0000

    edit and delete cotegory does not work yet

--- app.py
+++ app.py
@@ -35,6 +35,47 @@ def edit_recip(recip_id):
     return render_template('editrecip.html', recip=the_recip,
                            categories=all_categories)
 
+@app.route('/update_recip/<recip_id>', methods=["POST"])
+def update_recip(recip_id):
+    recip = mongo.db.recip
+    recip.update( {'_id': ObjectId(recip_id)},
+    {
+        'recip_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'recip_description': request.form.get('task_description'),
+        'gluten_free':request.form.get('gluten_free')
+    })
+    return redirect(url_for('get_recip'))
+
+@app.route('/delete_recip/<recip_id>')
+def delete_recip(recip_id):
+    mongo.db.recip.remove({'_id': ObjectId(recip_id)})
+    return redirect (url_for('get_recips'))
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+    categories=mongo.db.categories.find())
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one(
+                           {'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
